b4956db8e2f06ecf8f11b4a74cd7bbcff2846f42
Duke0fCodes <deek5h17h.c@gmail.com>
2024-05-19 20:52:13 +0530

    hamburger fix

--- script.js
+++ script.js
@@ -3,18 +3,26 @@ document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.getElementById('hamburger');
   const navLinks = document.getElementById('nav-links');
   const body = document.querySelector('body');
-  
+  $(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        items: 1
+    });
+});
   hamburger.addEventListener('click', () => {
-      navbar.classList.toggle('active');
-      navLinks.classList.toggle('active');
-      
-      // Toggle body scroll
-      if (navLinks.classList.contains('active')) {
-          body.style.overflow = 'hidden';
-      } else {
-          body.style.overflow = '';
-      }
-  });
+    navbar.classList.toggle('active');
+    navLinks.classList.toggle('active');
+        if (navLinks.classList.contains('active')) {
+        body.style.overflow = 'hidden';
+    } else {
+        body.style.overflow = '';
+    }
+});
   window.addEventListener("scroll", function() {
     var navbar = document.getElementById("navbar");
     var logo = document.querySelector(".logo img");
