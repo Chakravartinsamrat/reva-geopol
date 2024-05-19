d473daf8b325b48792556a55d28ba91710d4e6df
Duke0fCodes <deek5h17h.c@gmail.com>
2024-05-19 20:28:10 +0530

    nav transition max

--- script.js
+++ script.js
@@ -15,4 +15,16 @@ document.addEventListener('DOMContentLoaded', () => {
           body.style.overflow = '';
       }
   });
+  window.addEventListener("scroll", function() {
+    var navbar = document.getElementById("navbar");
+    var logo = document.querySelector(".logo img");
+    var hamburger = document.querySelector(".hamburger");
+
+    navbar.classList.toggle("sticky", window.scrollY > 0);
+    navbar.style.backgroundColor = window.scrollY > 5 ? '#17a2b8' : 'transparent';
+    logo.style.height = window.scrollY > 5 ? '60px' : '70px';
+    logo.style.marginTop = window.scrollY > 5 ? '20px' : '10px';
+    logo.style.marginBottom = window.scrollY > 5 ? '20px' : '10px';
+});
+
 });
